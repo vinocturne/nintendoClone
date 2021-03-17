ae6ffa557c3852582870cc1654be98dd883ff8e6
vinocturne <vinocturne@naver.com>
2021-03-17 18:11:09 +0900

    Initial commit

---
+++ src/main/java/com/clone/nintendo/NintendoApplication.java
@@ -0,0 +1,13 @@
+package com.clone.nintendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NintendoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NintendoApplication.class, args);
+    }
+
+}
